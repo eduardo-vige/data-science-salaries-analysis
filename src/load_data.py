02a52e9486832ce6699e436dfb25fd1a98bbb36c
Eduardo-Notebook <dev.eduardovige.com>
2026-03-13 14:02:48 -0300

    Criando EDA inicial

--- src/load_data.py
+++ src/load_data.py
@@ -2,13 +2,15 @@ import kagglehub
 import os
 import shutil
 
-existe_df = os.path.exists("data/raw/DataScience_salaries_2025.csv")
+def load_data():
 
-if(not existe_df):
-
-    # Download latest version
-    path = kagglehub.dataset_download("saurabhbadole/latest-data-science-job-salaries-2024")
-
-    # Copy to data
     existe_df = os.path.exists("data/raw/DataScience_salaries_2025.csv")
-    shutil.copy(f"{path}/DataScience_salaries_2025.csv", "data/raw/DataScience_salaries_2025.csv")
+
+    if(not existe_df):
+
+        # Download latest version
+        path = kagglehub.dataset_download("saurabhbadole/latest-data-science-job-salaries-2024")
+
+        # Copy to data
+        existe_df = os.path.exists("data/raw/DataScience_salaries_2025.csv")
+        shutil.copy(f"{path}/DataScience_salaries_2025.csv", "data/raw/DataScience_salaries_2025.csv")
